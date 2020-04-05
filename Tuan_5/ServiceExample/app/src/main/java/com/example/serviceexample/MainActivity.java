93feefce118e9f5edd022014e6c54c4857326a81
letranbaosuong <letranbaosuong@gmail.com>
2020-04-05 08:51:03 +0700

    done Tuần 5 - Service Example

---
+++ Tuan_5/ServiceExample/app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.serviceexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View v) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View v) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
